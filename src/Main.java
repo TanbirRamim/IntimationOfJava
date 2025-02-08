f24695a48bcee601f6f1d0b5bd5be877f32014cc
tanbirramim <tanbirramim420@gmail.com>
2025-02-09 00:31:13 +0100

    day completed

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+public static void main(String[] args){
+    System.out.println("Hello World");
+    int a = 20;
+    int b = 10;
+    int sum = a+b;
+    System.out.println("Sum is: "+ sum);
+    System.out.print("I love Mangoooo! ");
+}
+}
